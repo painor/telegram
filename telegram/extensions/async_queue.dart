596bcdc62743a26519d3103e8d6b93971626c666
painor <30215770+painor@users.noreply.github.com>
2020-08-09 00:19:48 +0100

    Add AsyncQueue class

---
+++ telegram/extensions/async_queue.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+class AsyncQueue {
+  List<List<int>> _queue;
+  Completer<bool> canPush = new Completer<bool>();
+  Completer<bool> canGet = new Completer<bool>();
+
+  AsyncQueue() {
+    this._queue = [];
+    this.canPush.complete(true);
+  }
+
+  Future<void> push(List<int> value) async{
+    await this.canPush;
+    this._queue.add(value);
+    this.canGet.complete(true);
+    this.canPush = new Completer();
+  }
+
+  Future<List<int>> pop() async{
+    await this.canGet;
+    final List<int> returned = this._queue.removeLast();
+    this.canPush.complete(true);
+    this.canGet = new Completer<bool>();
+    return returned;
+  }
+}

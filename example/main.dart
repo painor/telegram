684d8fbecfbcef3a3cbdfd166f3186f93f5830db
Chris Watson <cawatson1993@gmail.com>
2021-08-19 03:23:49 -0600

    Start moving user facing logic into TelegramClient

--- example/main.dart
+++ example/main.dart
@@ -1,11 +1,7 @@
 import '../telegram/client/telegram_client.dart';
 
-class A {
-  static int b = 5;
-}
-
 main() async {
-  var client = TelegramClient("aa", 1, "a");
+  var client = TelegramClient("aa", 1, "a", {});
   await client.connect();
   print("Connected successfully");
 }
